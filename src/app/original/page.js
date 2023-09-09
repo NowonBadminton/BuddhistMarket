ee1ba3535c1912b54213464eb6ac82d833daae1f
judiking1 <judiking1@gmail.com>
2023-09-09 23:38:05 +0900

    original page first commit

--- src/app/original/page.js
+++ src/app/original/page.js
@@ -1,12 +1,80 @@
+import Link from "next/link";
+import Image from 'next/image';
+
 export const metadata = {
     title: 'Original',
 };
 
 export default function Original () {
     return (
-        <>
-            Original Paintings
-        </>
+        <div className="w-full max-h-screen h-screen">
+            <div className="flex h-1/2">
+                {top3.map(item=>(
+                    <Link href={`/original/${item.alt}`} className="w-1/3 hover:opacity-75 transition-opacity relative" key={item.id}>
+                        <Image src={item.url} alt={item.alt} layout="fill" objectFit="cover"/>
+                    </Link>
+                ))}
+            </div>
+            <div className="flex h-1/2">
+                {after4.map(item=>(
+                    <Link href={`/original/${item.alt}`} className="hover:opacity-75 transition-opacity w-1/4 relative" key={item.id}>
+                        <Image src={item.url} alt={item.alt} layout="fill" objectFit="cover"/>
+                    </Link>
+                ))}
+            </div>
+        </div>
     )
-
 }
+
+const original = [
+    {
+    id: 1,
+    type: 'original',
+    alt: '1',
+    url: '/images/1.jpg',
+    },
+    {
+    id: 2,
+    type: 'original',
+    alt: '2',
+    url: '/images/2.jpg',
+    },
+    {
+    id: 2,
+    type: 'original',
+    alt: '2',
+    url: '/images/2.jpg',
+    },
+    {
+    id: 3,
+    type: 'original',
+    alt: '3',
+    url: '/images/3.jpg',
+    },
+    {
+    id: 4,
+    type: 'original',
+    alt: '4',
+    url: '/images/4.jpg',
+    },
+    {
+    id: 5,
+    type: 'original',
+    alt: '5',
+    url: '/images/5.jpg',
+    },
+    {
+    id: 6,
+    type: 'original',
+    alt: '6',
+    url: '/images/6.jpg',
+    },
+    {
+    id: 7,
+    type: 'original',
+    alt: '7',
+    url: '/images/7.jpg',
+    },
+];
+const top3 = original.slice(0, 3);
+const after4 = original.slice(3, 7);
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="bg-gradient-to-t from-gray-300 to-black min-h-screen w-full flex justify-center items-center">
-      <img src="/images/m7.jpg" alt="mainImage" className="w-full max-h-screen object-cover opacity-80"></img>
+      <img src="/images/main.jpg" alt="mainImage" className="w-full max-h-screen object-cover opacity-80"></img>
     </div>
   )
 }
