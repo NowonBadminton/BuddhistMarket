50ba8c11ccd126709579743747737f0018ca3af8
judiking1 <judiking1@gmail.com>
2023-09-12 18:35:32 +0900

    third commit home

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 export default function Header() {
     return (
-        <header className='flex justify-between px-5 py-5 bg-black text-white sticky w-full top-0 z-10'>
-            <div className='logo'>
+        <header className="flex justify-between px-5 py-5 bg-black text-white sticky w-full top-0 z-10">
+            <div className="logo">
                 <Link href={"/"}>불화마켓</Link>
             </div>
-            <nav className='flex gap-3'>
+            <nav className="flex gap-3">
                 <Link href={"/"}>Home</Link>
                 <Link href={"/original"}>Original</Link>
                 <Link href={"/copy"}>Copy</Link>
