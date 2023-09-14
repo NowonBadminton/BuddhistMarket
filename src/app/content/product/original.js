e03c2091e98cc9beffef059245c31060c613d90e
judiking1 <judiking1@gmail.com>
2023-09-14 20:35:28 +0900

    add product folder with original images

---
+++ src/app/content/product/original.js
@@ -0,0 +1,44 @@
+export const original = [
+    {
+    id: 1,
+    type: 'original',
+    alt: '1',
+    url: '/images/1.jpg',
+    },
+    {
+    id: 2,
+    type: 'original',
+    alt: '2',
+    url: '/images/2.jpg',
+    },
+    {
+    id: 3,
+    type: 'original',
+    alt: '3',
+    url: '/images/3.jpg',
+    },
+    {
+    id: 4,
+    type: 'original',
+    alt: '4',
+    url: '/images/4.jpg',
+    },
+    {
+    id: 5,
+    type: 'original',
+    alt: '5',
+    url: '/images/5.jpg',
+    },
+    {
+    id: 6,
+    type: 'original',
+    alt: '6',
+    url: '/images/6.jpg',
+    },
+    {
+    id: 7,
+    type: 'original',
+    alt: '7',
+    url: '/images/7.jpg',
+    },
+];
